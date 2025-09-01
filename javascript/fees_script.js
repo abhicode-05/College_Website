20f3702e17b514aee5f04425fa3b5d5ead2ed639
Abhishek Tiwari <abhishek.tiwari01205@gmail.com>
2025-09-01 20:02:22 +0530

    Add files via upload

---
+++ javascript/fees_script.js
@@ -0,0 +1,190 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==1){
+        document.getElementById("demo").innerHTML ="AAKASH KUMAR SINGH";
+        document.getElementById("demo1").innerHTML ="10000";
+    }
+    else if(inputVal==2)
+    {
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="ABHISHEK TIWARI";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="AKASH SINGH";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==5)
+    {
+        document.getElementById("demo").innerHTML ="ADITYA RAWAT";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==6)
+    {
+        document.getElementById("demo").innerHTML ="VISHAL KUMAR GOLA";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="KUNAL SHARMA";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="ROHAN";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==9)
+    {
+        document.getElementById("demo").innerHTML ="ANIL";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="AASHUTOSH SHARMA";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="RUDRA KAPOOR";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==12)
+    {
+        document.getElementById("demo").innerHTML ="GULSHAN GUPTA";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="MEHAK GUPTA";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="MAHMILFATEMA PARVEZALI KADIWALA";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="ADANAN ABDULAJIJ MAKNOJIYA";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="MOHAMMADADIL MAHEBUBKHAN MOGAL";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="MANSIBEN HARESHBHAI NAYI";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="AZRUDDIN ASLAMBHAI PALASARA";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="ALPESHKUMAR BALUBHAI PARMAR";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="ROHITKUMAR JASVANTBHAI PARMAR";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==21)
+    {
+        document.getElementById("demo").innerHTML ="RAVIKUMAR ASHOKBHAI PATEL";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==22)
+    {
+        document.getElementById("demo").innerHTML ="SAHILKUMAR ARVINDBHAI PATEL";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==23)
+    {
+        document.getElementById("demo").innerHTML ="SAKSHI KIRTIKUMAR PATEL";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==24)
+    {
+        document.getElementById("demo").innerHTML ="DIGESH KIRTIBHAI PRAJAPATI";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==25)
+    {
+        document.getElementById("demo").innerHTML ="SHAILESHKUMAR NARESHBHAI PRAJAPATI";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==26)
+    {
+        document.getElementById("demo").innerHTML ="VIKASHBHAI NARESHBHAI PRAJAPATI";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+     else if(inputVal==27)
+    {
+        document.getElementById("demo").innerHTML ="KRUPAL SHAILESHKUMAR RAJPUT";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==28)
+    {
+        document.getElementById("demo").innerHTML ="MAHAMAD HANIF FARUKBHAI SAIYED";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+
+    else if(inputVal==29)
+    {
+        document.getElementById("demo").innerHTML ="DIVY CHANDRAKANT SHUKLA";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+     else if(inputVal==30)
+    {
+        document.getElementById("demo").innerHTML ="JAYDIPSINH JASHVANTSINH SOLANKI";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==31)
+    {
+        document.getElementById("demo").innerHTML ="MOHAMMADMISAM ALIAKBAR SUNASARA";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+    else if(inputVal==32)
+    {
+        document.getElementById("demo").innerHTML ="MUSKANFATEMA NASIMHAIDAR SUNASARA";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==33)
+    {
+        document.getElementById("demo").innerHTML ="SAHILBHARATHI HIRABHARATHI SWAMI";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==34)
+    {
+        document.getElementById("demo").innerHTML ="NONSINGJI VANARAJJI THAKOR";
+        document.getElementById("demo1").innerHTML ="20000 + 2500 = 22500"; 
+    }
+    else if(inputVal==35)
+    {
+        document.getElementById("demo").innerHTML ="ADITYA SURESHBHAI TONGDA";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
+
